5b8888d93e05efcf1764af1f55fe188e89934d5c
Baran <st156502@stud.uni-stuttgart.de>
2022-04-18 14:37:16 +0200

    PIME-13 fixing naming convention and imports

--- pime2/sensor/button_listener.py
+++ pime2/sensor/button_listener.py
@@ -1,4 +1,4 @@
-import RPi.GPIO as GPIO
+from RPi import GPIO
 from pime2.log import pime_logger
 import time
 
@@ -12,30 +12,30 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(button1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(button2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-isButtonOneUp = False
-isButtonTwoUp = False
+IS_BUTTON_ONE_UP = False
+IS_BUTTON_TWO_UP = False
 
 #Start listening
 try:
     while True:
         if GPIO.input(button1) == GPIO.HIGH:
             pime_logger.logging.info("Knopf 1 unten")
-            isButtonOneUp = False
+            IS_BUTTON_ONE_UP = False
             time.sleep(2.0)
         if GPIO.input(button1) == GPIO.LOW:
             pime_logger.logging.info("Knopf 1 oben")
-            isButtonOneUp = True
+            IS_BUTTON_ONE_UP = True
             time.sleep(2.0)
         if GPIO.input(button2) == GPIO.LOW:
             pime_logger.logging.info("Knopf 2 oben")
-            isButtonTwoUp = True
+            IS_BUTTON_TWO_UP = True
             time.sleep(2.0)
         if GPIO.input(button2) == GPIO.HIGH:
             pime_logger.logging.info("Knopf 2 unten")
-            isButtonTwoUp = False
+            IS_BUTTON_TWO_UP = False
             time.sleep(2.0)
 except KeyboardInterrupt:
     #Ending sensor listening with ctrl+c
     pime_logger.logging.info('Ending sensor listening')
-    if isButtonOneUp == False:
+    if IS_BUTTON_ONE_UP is False:
         print('button one not up')
