223ce6664030c99af1b48d9ab16fd5989c1d6fb6
Seval Avdic <st155243@stud.uni-stuttgart.de>
2022-04-23 17:47:13 +0200

    Healt metrics as JSON response added

--- pime2/res/health.py
+++ pime2/res/health.py
@@ -1,4 +1,7 @@
+import json
 import aiocoap
+import psutil
+import os
 from aiocoap import resource
 
 
@@ -14,4 +17,29 @@ class Health(resource.Resource):
         :param request:
         :return:
         """
-        return aiocoap.Message(payload=b"Health")
+
+        pime_version = "0.0.1"
+        cpu_core_count = os.cpu_count()
+        cpu_usage = psutil.cpu_percent()
+        ram_availabe = psutil.virtual_memory().available
+        ram_used = psutil.virtual_memory().used
+        ram_used_percentage = psutil.virtual_memory().percent
+        neighbour_count = 0
+        sensor_count = 0
+        actuator_count = 0
+
+        response_dict = {
+            "PIME version": pime_version,
+            "CPU core count": cpu_core_count,
+            "CPU usage": cpu_usage,
+            "RAM available": ram_availabe,
+            "RAM used": ram_used,
+            "RAM used Percentage": ram_used_percentage,
+            "Neighbour count": neighbour_count,
+            "Sensor count": sensor_count,
+            "Actuator count": actuator_count
+        }
+
+        response_json = json.dumps(response_dict)
+
+        return aiocoap.Message(payload=response_json.encode())
