b483149cc6f7f5fc7a277df4c7fa75280f29f1cf
Baran <st156502@stud.uni-stuttgart.de>
2022-04-23 08:00:35 +0200

    PIME 13 and 17 fixing linter

--- pime2/sensor/temperature_dummy.py
+++ pime2/sensor/temperature_dummy.py
@@ -7,5 +7,5 @@ def temperature_dummy():
     Temp sensor dummy
     """
     random_temperature = random.randint(-50, 50)
-    logging.info("Temp " + str(random_temperature))
+    logging.info("Temp: {}".format(random_temperature))
     return random_temperature
