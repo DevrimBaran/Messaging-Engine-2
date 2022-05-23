bd9bcbcc467ff77c8a11c6d6c8cb3b18fe57cffa
Marius Schuppert <st155092@stud.uni-stuttgart.de>
2022-05-24 00:17:12 +0200

    me-6: add node entity class, implement node put endpoint, add node create event,
    Signed-off-by: Marius Schuppert <st155092@stud.uni-stuttgart.de>

--- pime2/res/node.py
+++ pime2/res/node.py
@@ -1,11 +1,48 @@
+import json
+import logging
+from json import JSONDecodeError
+
 import aiocoap
 from aiocoap import resource
+
+from pime2.message import NodeCreateResultMessage
+from pime2.model.node import NodeEntity
+from pime2.push_queue import get_push_queue
 
 
 class Node(resource.Resource):
     """
     Node Resource
     """
+
+    async def render_put(self, request):
+        """
+        this method assumes json formatted input of a node record
+
+        :param request:
+        :return:
+        """
+        required_fields = [
+            "name",
+            "ip",
+            "port",
+        ]
+        # parse node record from request payload - if possible
+        try:
+            if len(request.payload) > 2048:
+                return aiocoap.Message(payload=b"INVALID REQUEST")
+            node = json.loads(request.payload)
+
+            for i in required_fields:
+                if i not in node or node[i] is None:
+                    return aiocoap.Message(payload=b"INVALID REQUEST, MISSING PROPERTY")
+            node_record = NodeEntity(0, node["name"], node["ip"], node["port"])
+            await get_push_queue().put(json.dumps(NodeCreateResultMessage(node_record).__dict__))
+        except JSONDecodeError as ex:
+            logging.warning("Problem processing request: %s", ex)
+            return aiocoap.Message(payload=b"INVALID REQUEST")
+
+        return aiocoap.Message(payload=b"OK")
 
     async def render_get(self, request):
         """
