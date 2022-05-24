991b06c4f90231716f8dd2735510ef4095e88eb6
Baran <st156502@stud.uni-stuttgart.de>
2022-05-24 19:15:02 +0200

    updating led

--- pime2/actuator/led_actuator.py
+++ pime2/actuator/led_actuator.py
@@ -1,27 +1,25 @@
 # pylint: disable=import-outside-toplevel
 import logging
 
-from pime2.actuator.actuator import DualGpioActuator, ActuatorType
-from pime2.common.operator import DualGpioOperatorArguments
+from pime2.actuator.actuator import SingleGpioActuator, ActuatorType
+from pime2.common.operator import SingleGpioOperatorArguments
 
 
-class Led(DualGpioActuator):
+class Led(SingleGpioActuator):
     """
     A simple led actuator with two colors.
     input_arguments provide a property is_test_mode.
     """
 
-    def __init__(self, name: str, input_arguments: DualGpioOperatorArguments):
+    def __init__(self, name: str, input_arguments: SingleGpioOperatorArguments):
         super().__init__(name, ActuatorType.LED, input_arguments)
         self.green_led = input_arguments.input_gpio_1
-        self.red_led = input_arguments.input_gpio_2
         self.args = input_arguments
         self.green_led_on = False
         self.red_led_on = False
 
-    def activate(self, input_arg_one: bool, input_arg_two: bool):
+    def activate(self, input_arg_one: bool):
         led_green = input_arg_one
-        led_red = input_arg_two
         if self.args.is_test_mode is False:
             from RPi import GPIO
             if led_green is True:
@@ -29,27 +27,20 @@ class Led(DualGpioActuator):
                 self.green_led_on = True
                 logging.info("Green led is on")
                 GPIO.output(self.green_led, GPIO.HIGH)
-            if led_green is True:
-                # LED wird eingeschaltet
-                self.red_led_on = True
-                logging.info("Red led is on")
-                GPIO.output(self.green_led, GPIO.HIGH)
         else:
-            logging.info("Green led: %s, Red led: %s", str(led_green), str(led_red))
+            logging.info("Green led: %s", str(led_green))
             self.green_led_on = led_green
-            self.red_led_on = led_red
 
     def open(self):
         if self.args.is_test_mode is False:
             from RPi import GPIO
             GPIO.setmode(GPIO.BCM)
-            GPIO.setup(self.red_led, GPIO.OUT, initial=GPIO.LOW)
             GPIO.setup(self.green_led, GPIO.OUT, initial=GPIO.LOW)
 
     def close(self):
         if self.args.is_test_mode is False:
             from RPi import GPIO
-            GPIO.cleanup((self.green_led, self.red_led))
+            GPIO.cleanup(self.green_led)
         self.green_led_on = False
         self.red_led_on = False
         logging.info("Led is off")
