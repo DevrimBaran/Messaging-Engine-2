508ca00a57e7ae50d5e85fcc2f307ca244de0537
Enes Oezver <muhammed.oezver@hotmail.de>
2022-05-26 15:57:16 +0200

    PIME-35 fix linter

--- pime2/neighbour.py
+++ pime2/neighbour.py
@@ -1,33 +1,37 @@
+# pylint: disable=W1203
+# pylint: disable=W0703
 import logging
 import time
+import socket
 import pime2.coap_client as coap_client
 
-"""
-Finds all available hosts
-Note: In Windows you have to give the subnet as parameter into find_neighbours. Example: find_neighbours("192.168.30.")
-"""
-async def find_neighbours(custom_Subnet = None):
+
+async def find_neighbours(custom_subnet = None):
+    """
+    Finds all available hosts
+    Note: In Windows you have to give the subnet as parameter into find_neighbours.
+    Example: find_neighbours("192.168.30.")
+    """
     available_ip = []
 
-    for x in range(1, 255):
+    for suffix in range(1, 255):
+        if custom_subnet:
+            subnet = custom_subnet
+        else:
+            subnet = find_local_subnet()
+
+        target = subnet + str(suffix)
+
+        logging.info('Starting scan on host: %s', target)
+
+        start = time.time()
         try:
-            if (custom_Subnet):
-                subnet = custom_Subnet
-            else:
-                subnet = find_local_subnet()
-            
-            target = subnet + str(x)
-
-            logging.info('Starting scan on host: %s', target)
-
-            start = time.time()
-
             is_ping_successful = await coap_client.ping(target)
-            if (is_ping_successful):
+            if is_ping_successful:
                 available_ip.append(target)
             else:
                 logging.info("No device on: %s", target)
-        
+
         except Exception as exception:
             logging.error("Error while searching for neighbours: %s", exception)
         finally:
@@ -40,19 +44,13 @@ async def find_neighbours(custom_Subnet = None):
     return available_ip
 
 
-"""
-Extracts the local subnet from the local ip of the host.
-Works not with Windows as expected.
-"""
+
 def find_local_subnet():
+    """
+    Extracts the local subnet from the local ip of the host.
+    Works not with Windows as expected.
+    """
     hostname = socket.gethostname()
-    local_IP = socket.gethostbyname(hostname)
-    local_subnet = ".".join(local_IP.split(".")[:-1])
+    local_ip = socket.gethostbyname(hostname)
+    local_subnet = ".".join(local_ip.split(".")[:-1])
     return local_subnet
-
-
-
-
-
-
-
