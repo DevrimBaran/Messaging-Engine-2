41c68989d3805ab5756f818541ba716d7d5e638d
Sheki <s_avdic@gmx.de>
2022-05-27 19:34:26 +0200

    Implement get id method, refactor delete all method

--- pime2/repository/NodeRepository.py
+++ pime2/repository/NodeRepository.py
@@ -1,6 +1,8 @@
+from typing import List
+
 import pime2.database as db
 import logging
-import pime2.node
+import pime2.entity.node
 import sqlite3
 
 class NodeRepository():
@@ -8,7 +10,7 @@ class NodeRepository():
     def __init__(self):
         self.connection = db.create_connection("pime_database.db")
 
-    def create_node(self, node : pime2.node.NodeEntity):
+    def create_node(self, node : pime2.entity.node.NodeEntity):
         cursor = self.connection.cursor()
         query = 'INSERT INTO nodes(name,ip,port) VALUES(?,?,?);' 
         logging.debug('Executing SQL query: "%s"', query)
@@ -22,7 +24,7 @@ class NodeRepository():
             self.commit()
             cursor.close()
 
-    def read_node_by_name(self, name : str) -> pime2.node.NodeEntity:
+    def read_node_by_name(self, name : str) -> pime2.entity.node.NodeEntity:
         cursor = self.connection.cursor()
         query = 'SELECT * FROM nodes WHERE name = ?;'
         logging.debug('Executing SELECT SQL query: "%s" with name:<%s>', query, name)
@@ -32,12 +34,28 @@ class NodeRepository():
             logging.debug('No node with name "%s" exists.', name)
             return None
         logging.debug('Query executed. Result: %s', node_in_database)
-        result_node = pime2.node.NodeEntity(node_in_database[1],node_in_database[2],node_in_database[3]) 
+        result_node = pime2.entity.node.NodeEntity(node_in_database[1],node_in_database[2],node_in_database[3]) 
         cursor.close()
         return result_node
+    
+    def read_all_nodes(self) -> List[pime2.entity.node.NodeEntity]:
+        cursor = self.connection.cursor()
+        query = 'SELECT * FROM nodes;'
+        logging.debug('Executing SELECT SQL query: "%s"', query)
+        cursor.execute(query)
+        nodes_in_database = cursor.fetchall()
+        if nodes_in_database is None:
+            logging.debug('No nodes existing.')
+            return None
+        logging.debug('Query executed. Result: %s', nodes_in_database)
+        cursor.close()
+        result__list = []
+        for node in nodes_in_database:
+            result__list.append(pime2.entity.node.NodeEntity(node[1],node[2],node[3]))
+        return result__list
      
 
-    def update_node(self, node : pime2.node.NodeEntity)-> pime2.node.NodeEntity:
+    def update_node(self, node : pime2.entity.node.NodeEntity)-> pime2.entity.node.NodeEntity:
         cursor = self.connection.cursor()
         query = """ 
         UPDATE nodes
@@ -72,13 +90,26 @@ class NodeRepository():
 
     def delete_all(self):
         cursor = self.connection.cursor()
-        query = 'DELETE FROM nodes;'
+        query = 'DELETE FROM nodes WHERE id != 1;'
         logging.debug('Executing DELETE ALL SQL query: "%s"', query)
         cursor.execute(query)
         self.commit()
         logging.debug('Deleted all records from table "node"')
         cursor.close()
     
+    def get_node_id_by_name(self, name) -> int:
+        cursor = self.connection.cursor()
+        query = 'SELECT id FROM nodes WHERE name = ?;'
+        logging.debug('Executing SELECT SQL query: "%s" with name:<%s>', query, name)
+        cursor.execute(query, (name,))
+        node_id = cursor.fetchone()
+        if node_id is None:
+            logging.debug('No node with name "%s" exists.', name)
+            return None
+        logging.debug('Query executed. Result: id:<%s>', node_id[0])
+        cursor.close()
+        return node_id[0]
+
     def open_repository(self):
         self.connection = db.create_connection("pime_database.py")
     
