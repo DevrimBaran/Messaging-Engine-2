cbcf28a6c6f381ff1b3e784ec2004695f47cf929
Sheki <s_avdic@gmx.de>
2022-05-28 16:30:37 +0200

    Implement put method

--- pime2/res/hello.py
+++ pime2/res/hello.py
@@ -1,11 +1,25 @@
 import aiocoap
 from aiocoap import resource
+from pime2.repository.NodeRepository import NodeRepository
+from pime2.service.NodeService import NodeService
+from aiocoap import Code, Message
 
 
 class Hello(resource.Resource):
     """
     Hello Resource
     """
+    def __init__(self):
+        self.node_service = NodeService()
+
+    async def render_put(self, request):
+        handle_node_message = await self.node_service.handle_incoming_node(request)
+        if handle_node_message.code == Code.CREATED:
+            my_node = self.node_service.get_own_node()
+            my_node_json = self.node_service.entity_to_json(my_node)
+            return Message(payload=my_node_json.encode())
+        else:
+            return handle_node_message
 
     async def render_get(self, request):
         """
@@ -14,4 +28,4 @@ class Hello(resource.Resource):
         :param request:
         :return:
         """
-        return aiocoap.Message(payload=b"Helloooo!")
+        return Message(payload=b"Helloooo!")
