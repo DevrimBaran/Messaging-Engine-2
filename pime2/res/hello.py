9f8a2347e951a6d147d44661a561144ccea35bac
Seval Avdic <st155243@stud.uni-stuttgart.de>
2022-05-30 20:23:25 +0200

    Delete put endpoint

--- pime2/res/hello.py
+++ pime2/res/hello.py
@@ -11,15 +11,6 @@ class Hello(resource.Resource):
     def __init__(self):
         self.node_service = NodeService()
 
-    async def render_put(self, request):
-        """Handles incoming hello message and sends own node entity json as a response"""
-        handle_node_message = await self.node_service.handle_incoming_node(request)
-        if handle_node_message.code == Code.CREATED:
-            my_node = self.node_service.get_own_node()
-            my_node_json = self.node_service.entity_to_json(my_node)
-            return Message(payload=my_node_json.encode())
-        return handle_node_message
-
     async def render_get(self, request):
         """
         handle GET request to /hello
