542729c50a7b8cc5e72f67e18360d62faae02236
Seval Avdic <st155243@stud.uni-stuttgart.de>
2022-06-04 20:33:53 +0200

    Refactor Code

--- pime2/res/node.py
+++ pime2/res/node.py
@@ -1,14 +1,14 @@
 import logging
 from aiocoap import resource, Message
 from pime2.service.node_service import NodeService
-
+from pime2.database import get_db_connection
 class Node(resource.Resource):
     """
     Node Resource
     """
 
     def __init__(self):
-        self.node_service = NodeService()
+        self.node_service = NodeService(get_db_connection())
 
     async def render_put(self, request):
         """
