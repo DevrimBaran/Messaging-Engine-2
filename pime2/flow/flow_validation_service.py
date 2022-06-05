f1f59d6dc6fdc9c5331712a8d02f084e08edf859
Baran <st156502@stud.uni-stuttgart.de>
2022-06-05 16:21:56 +0200

    Adding flow validation

--- pime2/flow/flow_validation_service.py
+++ pime2/flow/flow_validation_service.py
@@ -1,4 +1,5 @@
-from typing import List
+import re
+from typing import List#
 
 from pime2.entity import FlowEntity
 
@@ -12,12 +13,30 @@ class FlowValidationService:
     def is_flow_valid(self, flow: FlowEntity) -> (bool, List[str]):
         """
         Method to check if a flow is valid.
-        TODO: Not yet implemented
 
         :param flow:
         :return:
         """
+        pattern = re.compile("^[a-zA-Z0-9_.-]{3,128}$")
         if len(flow.ops) == 0:
             return False, ["Got flow without operations!"]
-
+        elif not re.match(pattern, flow.name):
+            return False, ["Flow Name should match following regex: ^[a-zA-Z0-9_.-]{3,128}$!"]
+        elif len(flow.ops) <= 1:
+            return False, ["Got flow with too less operations!"]
+        for op in flow.ops:
+            if op.input != "sensor_temperature" or op.input != "sensor_hall" or op.input != "sensor_button" \
+                    or not op.input:
+                return False, ["Wrong input operation or input operation is not defined!"]
+            elif not op.where:
+                return False, ["Instance of where is not defined!"]
+            elif op.process != "log" or op.process != "cep_intercept" or not op.process:
+                return False, ["Wrong process name or process is empty!"]
+            elif "output" not in op:
+                return False, ["No output defined!"]
+            # TODO: Check in database if skill is present on instance (me-61 needs to be merged in)
+            elif not re.match(pattern, op.name):
+                return False, ["Operation Name should match following regex: ^[a-zA-Z0-9_.-]{3,128}$!"]
+            elif op.output != "exit" or op.output != "actuator_led" or op.output != "actuator_speaker" or not op.output:
+                return False, ["wrong input for output or output is empty!"]
         return True, []
