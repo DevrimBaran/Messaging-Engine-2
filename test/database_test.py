de562e572fb24ab74a31b9ac52524eb425b47473
Marius Schuppert <st155092@stud.uni-stuttgart.de>
2022-06-06 18:54:52 +0200

    me-41: fix database_test
    Signed-off-by: Marius Schuppert <st155092@stud.uni-stuttgart.de>

--- test/database_test.py
+++ test/database_test.py
@@ -1,9 +1,8 @@
 import unittest
 import os
-import pime2.database as db
 
 from pime2 import database
-from pime2.config import get_me_conf
+from pime2.config import get_me_conf, load_app_config
 from pime2.repository.node_repository import NodeRepository
 from pime2.service.node_service import NodeService
 
@@ -15,9 +14,10 @@ class DatabaseTest(unittest.TestCase):
     @classmethod
     def setUp(cls):
         if os.path.exists("testDatabase.db"):
-            db.disconnect(cls.connection)
+            database.disconnect(cls.connection)
             os.remove("testDatabase.db")
-        cls.connection = db.create_connection("testDatabase.db")
+        load_app_config("me.yaml")
+        cls.connection = database.create_connection("testDatabase.db")
         cls.node_repo = NodeRepository(cls.connection)
         database.create_default_tables(cls.connection, NodeService())
         cls.node_repo.delete_all()
