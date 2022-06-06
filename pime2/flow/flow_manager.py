3cf0212485533220c98a3d240517367451e43481
Baran <st156502@stud.uni-stuttgart.de>
2022-06-06 19:01:46 +0200

    Fix regex import and linter and node.py

--- pime2/flow/flow_manager.py
+++ pime2/flow/flow_manager.py
@@ -10,6 +10,7 @@ from pime2.config import get_me_conf
 from pime2.flow.flow_message_builder import FlowMessageBuilder
 from pime2.flow.flow_operation_manager import FlowOperationManager
 from pime2.entity import FlowEntity, FlowOperationEntity, FlowMessageEntity, NodeEntity
+from pime2.flow.flow_validation_service import is_flow_valid
 from pime2.sensor.sensor import SensorType
 from pime2.service.node_service import NodeService
 
@@ -60,7 +61,7 @@ class FlowManager:
         neighbors = self.get_nodes()
 
         # validate
-        is_valid, validation_msgs = self.flow_validation_service.is_flow_valid(flow)
+        is_valid, validation_msgs = is_flow_valid(flow)
         if not is_valid:
             logging.error("Problem with flow '%s'", flow.name)
             for i in validation_msgs:
@@ -99,7 +100,7 @@ class FlowManager:
         :return:
         """
         # validate
-        is_valid, validation_msgs = self.flow_validation_service.is_flow_valid(flow)
+        is_valid, validation_msgs = is_flow_valid(flow)
         if not is_valid:
             logging.error("Problem with flow '%s'")
             for i in validation_msgs:
@@ -144,7 +145,7 @@ class FlowManager:
         :return:
         """
         # validate
-        is_valid, validation_msgs = self.flow_validation_service.is_flow_valid(flow)
+        is_valid, validation_msgs = is_flow_valid(flow)
         if not is_valid:
             logging.error("Problem with flow '%s'")
             for i in validation_msgs:
