8f08bb42ae798d8f9fed95c80882e75d20b27791
Sheki <s_avdic@gmx.de>
2022-06-09 01:01:47 +0200

    me-44: Refactor flow repository code

--- pime2/repository/flow_repository.py
+++ pime2/repository/flow_repository.py
@@ -3,37 +3,36 @@ import logging
 from sqlite3 import IntegrityError, Error
 from typing import List, Optional
 from pime2.entity import FlowEntity
+from pime2.mapper.flow_mapper import FlowMapper
 
 
 class FlowRepository:
-    """Implements node repository class"""
+    """Implements flow repository class"""
 
     def __init__(self, connection):
         self.connection = connection
+        self.flow_mapper = FlowMapper()
 
     def create_flow(self, flow: FlowEntity):
-        """Saves a node in database"""
+        """Saves a flow in database"""
         cursor = self.connection.cursor()
-
+        flow_operations = self.flow_mapper.flow_operation_to_json(flow.ops)
         # delete a flow with an existing name like this to avoid old data
         if self.check_in_database(flow.name):
             self.delete_flow_by_name(flow.name)
-        #TODO: Flowoperations as json string 
-        query = 'INSERT INTO flows(name, flow_ops) VALUES(?);'
+        query = 'INSERT INTO flows(name, ops) VALUES(?,?);'
         logging.debug('Executing SQL query: "%s"', query)
-        logging.debug('Values inserted: name:<%s>',
-                      flow.name)
+        logging.debug('Values inserted: name:<%s> ops:<%s>',
+                      flow.name, flow_operations)
         try:
             cursor.execute(query, (
-               flow.name,))
+               flow.name, flow_operations))
             self.commit()
-        except IntegrityError:
-            logging.debug("Integrity error during insert detected")
         finally:
             cursor.close()
 
     def read_flow_by_name(self, name: str) -> Optional[FlowEntity]:
-        """Return a node with a specific name from database"""
+        """Return a flow with a specific name from database"""
         cursor = self.connection.cursor()
         query = 'SELECT * FROM flows WHERE name = ?;'
         logging.debug('Executing SELECT SQL query: "%s" with name:<%s>', query, name)
@@ -45,7 +44,7 @@ class FlowRepository:
                 logging.debug('No flow with name "%s" exists.', name)
                 return None
             logging.debug('Query executed. Result: %s', flow_in_database)
-            result_flow = FlowEntity(flow_in_database[1], flow_in_database[2])
+            result_flow = FlowEntity(flow_in_database[1], self.flow_mapper.json_to_flow_operation(flow_in_database[2]))
 
             return result_flow
         finally:
@@ -65,40 +64,39 @@ class FlowRepository:
                 return []
             logging.debug('Query executed. Result: %s', flows_in_database)
             result_list = []
-            #TODO: FlowOperation
             for flow in flows_in_database:
-                flow_operations = [] if list(flow[2]).__len__() == 0 else flow[2].split(",")
-                result_list.append(FlowEntity(flow[1], flow_operations))
+                result_list.append(FlowEntity(flow[1], self.flow_mapper.json_to_flow_operation(flow[2])))
             return result_list
         finally:
             cursor.close()
 
     def update_flow(self, flow: FlowEntity):
-        """Updates a specific node"""
+        """Updates a specific flow"""
+
         cursor = self.connection.cursor()
         query = """
         UPDATE flows
-        SET flow_operation = ?
+        SET ops = ?
         WHERE name = ?; 
         """
         try:
             if self.check_in_database(flow.name):
                 logging.debug('Current flow: <%s>', flow)
                 logging.debug('Executing UPDATE SQL query: "%s"', query)
-                logging.debug('Updating values to: flow_operation: <%s>', flow.ops)
+                logging.debug('Updating values to: ops: <%s>', flow.ops)
                 #TODO: FlowOperation 
-                cursor.execute(query, (
-                    json.dumps(flow.ops.__dict__)))
+                flow_operations = self.flow_mapper.flow_operation_to_json(flow.ops)
+                cursor.execute(query, (flow_operations, flow.name))
                 self.commit()
                 logging.debug('Updated record.')
             else:
                 logging.debug('Can not update non existing flow with name "%s".', flow.name)
-                raise Error("Can not update non existing node")
+                raise Error("Can not update non existing flow")
         finally:
             cursor.close()
 
     def delete_flow_by_name(self, name: str):
-        """Deletes a specific node by its name"""
+        """Deletes a specific flow by its name"""
         cursor = self.connection.cursor()
         if self.check_in_database(name):
             query = 'DELETE FROM flows WHERE name = ?;'
@@ -109,7 +107,7 @@ class FlowRepository:
             cursor.close()
         else:
             logging.debug('Can not delete non existing flow with name "%s".', name)
-            raise Error("Can not delete non existing node")
+            raise Error("Can not delete non existing flow")
 
     def delete_all(self):
         """Deletes the flow records of the flows table"""
@@ -126,5 +124,5 @@ class FlowRepository:
         self.connection.commit()
 
     def check_in_database(self, name: str) -> bool:
-        """Checks if a node with a specific name is in database"""
+        """Checks if a flow with a specific name is in database"""
         return self.read_flow_by_name(name) is not None
