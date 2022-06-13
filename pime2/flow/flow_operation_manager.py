aa2bc480c50771f6aa89fa027d34984f8bb6d568
Enes Oezver <muhammed.oezver@hotmail.de>
2022-06-13 18:49:19 +0200

    ME-79 fix cep

--- pime2/flow/flow_operation_manager.py
+++ pime2/flow/flow_operation_manager.py
@@ -134,10 +134,10 @@ class FlowOperationManager:
                     execution_repository.register_execution(flow_message.flow_id, flow_message.id)
 
                     if f.is_process():
-                        if f.process == "cep_operation":
+                        if f.process == "cep_intercept":
                             logging.info("Executing CEP evaluation in flow %s with step %s", flow.name, step)
-                            if not cep_executer(f.args.expression, f.args.variables, flow_message.payload):
-                                logging.debug("CEP evaluation failed in flow %s with step %s", flow.name, step)
+                            if not cep_executer(f.args["expression"], f.args["variables"], payload):                                
+                                logging.info("Cancelling flow: CEP evaluation failed in flow %s with step %s", flow.name, step)
                                 return None
 
                         if f.process == "log":
